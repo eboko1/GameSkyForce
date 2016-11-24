a55a401362c7af01350c1c279d1ea86ff04b8d17
Victoria Frantsuh <eboko1@gmail.com>
2016-11-24 23:23:25 +0200

    Initial commit

---
+++ src/fvi/ua/skyforse/Display.java
@@ -0,0 +1,27 @@
+package fvi.ua.skyforse;
+
+import javax.swing.*;
+
+/**
+ * Created by Vika on 24.11.2016.
+ */
+public class Display {
+   private  String title;
+    private int width;
+    private int height;
+
+    private JFrame frame;
+    public Display(String title, int width, int height){
+        this.title=title;
+        this.width=width;
+        this.height=height;
+    }
+ public  void createDisplay(){
+     frame= new JFrame(title);
+     frame.setSize(width,height);
+     frame.setVisible(true);
+     frame.setLocationRelativeTo(null);
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ }
+
+}
